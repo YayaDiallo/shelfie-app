37be0c22ad35290ad2c3c61934d4ec936ba1a330
Yaya Diallo <yayazdiallo@outlook.fr>
2025-07-06 23:44:57 +0200

    feat: implement RouteLayout component for navigation setup with dynamic theming

---
+++ app/_layout.tsx
@@ -0,0 +1,30 @@
+import { Colors } from '@/constants/Colors';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, useColorScheme } from 'react-native';
+
+export default function RouteLayout() {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme || 'light'];
+
+  return (
+    <>
+      <StatusBar style='auto' />
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: theme.navBackground },
+          headerTintColor: theme.title,
+        }}
+      >
+        <Stack.Screen name='index' options={{ title: 'Home' }} />
+        <Stack.Screen name='about' options={{ title: 'About' }} />
+        <Stack.Screen
+          name='contact'
+          options={{ title: 'Contact', headerShown: false }}
+        />
+      </Stack>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
