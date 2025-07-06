95ac92b58bdb29f672c0a8946c61696d2cfc5e19
Yaya Diallo <yayazdiallo@outlook.fr>
2025-07-06 23:44:00 +0200

    feat: add ThemedLogo component for dynamic logo support based on color scheme

---
+++ components/ThemedLogo.tsx
@@ -0,0 +1,10 @@
+import { Image, ImageProps, useColorScheme } from 'react-native';
+import DarkLogo from '@/assets/img/logo_dark.png';
+import LightLogo from '@/assets/img/logo_light.png';
+
+export default function ThemedLogo({ ...props }: ImageProps) {
+  const colorScheme = useColorScheme();
+  const logo = colorScheme === 'dark' ? DarkLogo : LightLogo;
+
+  return <Image source={logo} {...props} />;
+}
