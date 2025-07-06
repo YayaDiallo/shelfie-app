cc26bf43d76bfdb3add5b513f1a61977b02be1bd
Yaya Diallo <yayazdiallo@outlook.fr>
2025-07-06 23:45:15 +0200

    feat: add About screen

---
+++ app/about.tsx
@@ -0,0 +1,32 @@
+import { Colors } from '@/constants/Colors';
+import { Link } from 'expo-router';
+import { StyleSheet, Text, useColorScheme, View } from 'react-native';
+
+export default function About() {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme || 'light'];
+  return (
+    <View style={[styles.container, { backgroundColor: theme.background }]}>
+      <Text style={styles.title}>about page</Text>
+      <Link href='/' style={styles.link}>
+        Back Home
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1,
+  },
+});
