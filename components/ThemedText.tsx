ff16f623d15ff94990c2e58cc203cdb15881d7f7
Yaya Diallo <yayazdiallo@outlook.fr>
2025-07-07 09:51:14 +0200

    feat: add ThemedText component for dynamic text styling based on color scheme

---
+++ components/ThemedText.tsx
@@ -0,0 +1,26 @@
+import { Colors } from '@/constants/Colors';
+import {
+  StyleProp,
+  Text,
+  TextProps,
+  useColorScheme,
+  ViewStyle,
+} from 'react-native';
+
+interface ThemedTextProps {
+  style?: StyleProp<ViewStyle>;
+  title?: boolean;
+}
+
+export default function ThemedText({
+  style,
+  title = false,
+  ...props
+}: ThemedTextProps & TextProps) {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme || 'light'];
+
+  const textColor = title ? theme.title : theme.text;
+
+  return <Text style={[{ color: textColor }, style]} {...props} />;
+}
