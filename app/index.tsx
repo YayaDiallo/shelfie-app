fb3b7b179eaf4fd5322c72bcfb077ae7e1587d4b
Yaya Diallo <yayazdiallo@outlook.fr>
2025-07-07 09:50:42 +0200

    refactor: update Home screen to use Themed components for consistency

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,8 @@ import { ThemedView } from '@/components/ThemedView';
 import { Link } from 'expo-router';
 import { StyleSheet, Text } from 'react-native';
 import ThemedLogo from '@/components/ThemedLogo';
+import Spacer from '@/components/Spacer';
+import ThemedText from '@/components/ThemedText';
 
 // In this way we can import images without TypeScript errors "Cannot find module './..png' or its corresponding type declarations.ts(2307)"
 // const Logo = require('@/assets/images/img/logo_light.png');
@@ -9,14 +11,18 @@ import ThemedLogo from '@/components/ThemedLogo';
 export default function Home() {
   return (
     <ThemedView style={styles.container}>
-      <ThemedLogo style={styles.img} />
-      <Text style={styles.title}>The Number 1</Text>
-      <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
+      <ThemedLogo />
+      <Spacer height={20} />
+      <ThemedText style={styles.title} title>
+        The Number 1
+      </ThemedText>
+      <Spacer height={10} />
+      <ThemedText>Reading List App</ThemedText>
       <Link href='/about' style={styles.link}>
-        About Page
+        <ThemedText>About Page</ThemedText>
       </Link>
       <Link href='/contact' style={styles.link}>
-        Contact Page
+        <ThemedText>Contact Page</ThemedText>
       </Link>
     </ThemedView>
   );
@@ -31,9 +37,6 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: 'bold',
     fontSize: 18,
-  },
-  img: {
-    marginVertical: 20,
   },
   link: {
     marginVertical: 10,
