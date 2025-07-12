efbfdb4344769c3b5e93fff26c9d9d8727eabc5c
Yaya Diallo <yayazdiallo@outlook.fr>
2025-07-12 17:36:21 +0200

    feat: enhance ThemedView component with safe area support and dynamic theming

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -1,26 +1,45 @@
-import { Colors } from '@/constants/Colors';
 import {
+  SafeAreaView,
   StyleProp,
-  useColorScheme,
   View,
   ViewProps,
   ViewStyle,
 } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useTheme } from './useTheme';
 
 export function ThemedView({
   style,
+  safe = false,
   ...props
 }: {
   style?: StyleProp<ViewStyle>;
+  safe?: boolean;
 } & ViewProps) {
-  const colorScheme = useColorScheme();
-  const theme = Colors[colorScheme || 'light'];
+  const { theme } = useTheme();
+  const insets = useSafeAreaInsets();
+
   // We don't have to manually output the children here, as React automatically renders children passed to it.
+  if (!safe)
+    return (
+      <View
+        style={[
+          {
+            backgroundColor: theme.background,
+          },
+          style,
+        ]}
+        {...props}
+      />
+    );
+
   return (
-    <View
+    <SafeAreaView
       style={[
         {
           backgroundColor: theme.background,
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
         },
         style,
       ]}
