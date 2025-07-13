e2275f5d1a4d70d5f352fdebddf61b579a16867d
Yaya Diallo <yayazdiallo@outlook.fr>
2025-07-13 14:01:10 +0200

    feat: wrap layout in UserProvider for authentication context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Colors } from '@/constants/Colors';
+import { UserProvider } from '@/contexts/UserContext';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, useColorScheme } from 'react-native';
@@ -8,7 +9,7 @@ export default function RouteLayout() {
   const theme = Colors[colorScheme || 'light'];
 
   return (
-    <>
+    <UserProvider>
       <StatusBar style='auto' />
       <Stack
         screenOptions={{
@@ -20,7 +21,7 @@ export default function RouteLayout() {
         <Stack.Screen name='(dashboard)' options={{ headerShown: false }} />
         <Stack.Screen name='index' options={{ title: 'Home' }} />
       </Stack>
-    </>
+    </UserProvider>
   );
 }
 
