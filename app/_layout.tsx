c1098f319b550a6b1b7526ea23ffb5fa0a408f96
Yaya Diallo <yayazdiallo@outlook.fr>
2025-07-20 11:57:24 +0200

    feat: wrap RouteLayout in BooksProvider for improved context management

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Colors } from '@/constants/Colors';
 import { UserProvider } from '@/contexts/UserContext';
+import { BooksProvider } from '@/contexts/BooksContext';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, useColorScheme } from 'react-native';
@@ -10,17 +11,19 @@ export default function RouteLayout() {
 
   return (
     <UserProvider>
-      <StatusBar style='auto' />
-      <Stack
-        screenOptions={{
-          headerStyle: { backgroundColor: theme.navBackground },
-          headerTintColor: theme.title,
-        }}
-      >
-        <Stack.Screen name='(auth)' options={{ headerShown: false }} />
-        <Stack.Screen name='(dashboard)' options={{ headerShown: false }} />
-        <Stack.Screen name='index' options={{ title: 'Home' }} />
-      </Stack>
+      <BooksProvider>
+        <StatusBar style='auto' />
+        <Stack
+          screenOptions={{
+            headerStyle: { backgroundColor: theme.navBackground },
+            headerTintColor: theme.title,
+          }}
+        >
+          <Stack.Screen name='(auth)' options={{ headerShown: false }} />
+          <Stack.Screen name='(dashboard)' options={{ headerShown: false }} />
+          <Stack.Screen name='index' options={{ title: 'Home' }} />
+        </Stack>
+      </BooksProvider>
     </UserProvider>
   );
 }
